7992f8cd23d98c23e35f9239593646e2754db40d
nikaysd1ab <70700799+nikaysd1ab@users.noreply.github.com>
2020-10-30 11:15:41 +0100

    Add files via upload

---
+++ verhaalovermij.py
@@ -0,0 +1,10 @@
+wie ben ik jaar 
+
+Ik ben nikay ik kom uit hoorn 
+ik ben 18 oud en zat hiervoor op spinaker risdam.
+Het is enigzist lastig voor mij heir gezien het een druke school is wat ik niet gewend ben.
+andere verwachten hier van me dat ik vriendelijk ben en 
+behulpzaam.
+ik spreek voornamelijk dezelfde taal als de rest al speerk ik ook deels een paar talen die mischien niet: een beetje zuid afrikaans redelijk duits en een klein beetje grieks.
+voorderest ken ik ook css
+Ik verwacht van deze opleindeing progameren te leren 
