f2b0ddf35644d128e5ebabe40b3a3e7e213ef99b
nikaysd1ab <70700799+nikaysd1ab@users.noreply.github.com>
2020-10-30 11:11:06 +0100

    Add files via upload

---
+++ tekst apelecatsie verhaal over imigrant.py
@@ -0,0 +1,114 @@
+
+print("geboren in amirika arazona ")
+
+ print("gang gewled, ik loop weg,niet weg lopen")
+choice = input()
+if choice == 'ik loop weg':
+    print(" je loopt weg en er gebeurd niks")
+elif choice == 'niet weg lopen':
+    print(" ik word dood geschoten, start progamma opnieuw op")
+else:
+    print(choice, "print wat in")
+
+print ("ik ga weg uit Amerika ga ik met de boot of vliegtuig ")
+
+choice = input()
+if choice == 'boot':
+    print("ik kom in nederland ")
+elif choice == 'vliegtuig':
+    print("ik kom in nederland")
+else:
+    print(choice, "voer wat in")
+
+print("ga ik studeren of huur ik een huis en ga ik werken ")
+
+choice = input()
+if choice == 'huur een huis':
+    print("je huurt een mooi huis ")
+elif choice == 'ik ga studeren':
+    print("ik ga studeren maar eidig op straat om dat ik uitijndelijk niks kan betalen ")
+else:
+    print(choice, "ja rot nou maar op met je kak hoofd")
+
+
+print("ga ik studeren of blijf ik werken ")
+
+choice = input()
+if choice == 'ik blijf werken':
+    print("je word een maneger bij deen en gaat niet studeren, start progamma opnieuw op ")
+elif choice == 'ik ga studeren':
+    print(" pak de bus")
+else:
+    print(choice, "je gaat studeren ")
+
+
+
+print("neem ik een tussen jaar of ga ik door met school")
+
+choice = input()
+if choice == 'ik neem een tussen jaar':
+    print("ik neem een tussen jaar")
+elif choice == 'blijf op school':
+    print("door corona heb ik minder geld en raak ik op straat ondanks dat ik alles verkoop, start het progamma opnieuw op")
+else:
+    print(choice, "voer wat in")
+
+
+print("goedkoper wonen of moter verkopen")
+choice = input()
+if choice == 'moter verkopen':
+    print("je verkoopt je moter en kan blijfen wonen in het huis")
+elif choice == 'goedkoper wonen':
+    print("je gaat goedkoper wonnen")
+else:
+    print(choice, "voer wat in")
+
+print ("ga ik naar de sportschool")
+
+choice = input()
+if choice == 'ik ga niet':
+    print("ik ga veder met mijn leven, start progamma opnieuw op")
+elif choice == 'ik ga sporten':
+    print("ig ga sporten en ontmoet nikay")
+else:
+    print(choice, "bene gij echt zo dom ")
+
+print("vraag ik nikay om hulp met nederlands typen ")
+
+choice = input()
+if choice == 'ja':
+    print("ik vraag nikay voor hulp")
+elif choice == 'nee':
+    print("ik ga veder met mijn leven maar met minder goed engels")
+else:
+    print(choice, "ja rot nou maar op")
+
+
+print("hij raad mij aan om een curses te doen ")
+
+choice = input()
+if choice == 'ik doe het niet':
+    print("ik ga veder met mij leven, start progama opnieuw")
+elif choice == 'ik doe het':
+    print("ik doe de curses en leer beter nederlands typen")
+else:
+    print(choice, "ja rot nou maar op ")
+
+
+
+print("help ik nikay met het maken van de tekst ")
+
+choice = input()
+if choice == 'nee':
+    print("i am a huge asshole ")
+elif choice == 'ja':
+    print(" ik help hem en ga veder met mijn leven ")
+else:
+    print(choice, "voer wat in")
+
+
+    print("bedankt voor het gebruiken van de apllecatsie je hebt het einde gehaalg")
+
+
+
+
---
+++ tekst aplecatsie over mij .py
@@ -0,0 +1,40 @@
+
+print("dit gaat over nikay")
+
+ print("hoe oud ben ik,18,19,17")
+choice = input()
+if choice == '18':
+    print("goed")
+elif choice == '17':
+    print("fout")
+elif choice == '19':
+    print("fout")
+
+else:
+    print(choice, "print wat in")
+
+print ("welke soprt doe ik,rygby,american football,voetbal")
+
+choice = input()
+if choice == 'rugby':
+    print("goed ")
+elif choice == 'american football':
+    print("fout")
+elif choice == 'voetbal':
+    print("fout")
+
+else:
+    print(choice, "voer wat in")
+
+print("wat is mijn fav bier.amstel,hertog jan, heiniken")
+
+choice = input()
+if choice == 'amstel':
+    print("fout")
+elif choice == 'hertog jan':
+    print("goed")
+elif choice == 'heiniken':
+    print("fout")
+
+else:
+    print(choice, "ja rot nou maar op met je kak hoofd")
